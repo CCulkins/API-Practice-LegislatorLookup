91e97801565c37304e9675e60bde33dce4f607d3
cassandra culkins <Guest@epicodus-42.local>
2016-04-05 11:36:04 -0700

    adds committee page

---
+++ app/routes/committee.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    var url = 'http://congress.api.sunlightfoundation.com/committees?apikey=dcbe6f973f804bf28ab662c7f64a55a9';
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.results;
+    });
+  }
+});
